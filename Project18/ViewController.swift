84e6d31d9a983ac5e453e040cc52d3ce6c6935c6
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-10-06 00:35:59 +0200

    End of project
    Just an introduction to some basic debugging methods.

--- Project18/ViewController.swift
+++ Project18/ViewController.swift
@@ -11,9 +11,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("I'm inside the viewDidLoad() method!")
+        print(1, 2, 3, 4, 5, separator: "-")
+        
+        assert(1 == 1, "Maths failure!")
+        
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
     }
-
-
 }
-
